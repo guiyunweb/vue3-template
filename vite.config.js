e281f9723e829cd17ac4c497ac02fece487e6680
归云 <i@guiyunweb.com>
2022-09-05 16:11:09 +0800

    修改 vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
+import {fileURLToPath, URL} from 'node:url'
+
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite';
+import {VantResolver} from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [VantResolver()],
+    }),
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:11000",
+        // target: "http://152.69.194.37:11000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+      "/img/": {
+        // target: "http://127.0.0.1:11000",
+        target: "http://152.69.194.37:10000/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/img/, ""),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@api': fileURLToPath(new URL('./src/api', import.meta.url)),
+    }
+  }
 })
+
